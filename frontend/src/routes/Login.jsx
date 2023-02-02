afd8c7682cf6aebde21f122edb2548405ed75888
Mike Dougherty <mikedough213@gmail.com>
2023-02-01 22:26:14 -0600

    Create simple form with Formik

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,7 +1,29 @@
+import { Form, Field, Formik } from 'formik';
+
 function Login() {
+  const initialValues = {
+    username: '',
+    password: '',
+  };
+  const onSubmit = values => {
+    window.alert(
+      JSON.stringify(values, null, 4)
+    );
+  };
   return (
     <div>
       <h2>Login</h2>
+      <Formik {...{initialValues, onSubmit}}>
+        <Form>
+          <div> 
+            <Field name='username' type='text'/>
+          </div>
+          <div>
+            <Field name='password' type='password'/>
+          </div>
+          <input type="submit"/>
+        </Form>
+      </Formik>
     </div>
   );
 }
