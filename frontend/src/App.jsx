7cc5a9724aa9d7fa941b8034b438e00da9fe8b1c
Mike Dougherty <mikedough213@gmail.com>
2023-02-03 23:33:23 -0600

    Fix bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Routes, Route, Outlet, Link } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import { AppStyle } from './styles';
+import { AppStyle, AppHeader } from './styles';
 
 import './App.css';
 
@@ -44,12 +44,12 @@ const Layout = () => {
     <div>
       {/* A "layout route" is a good place to put markup you want to
           share across all the pages on your site, like navigation. */}
-      <nav className="App-header">
+      <AppHeader>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
         <Link to="/patients">Patients</Link>
         <Link to="/nothing-here">Nothing Here</Link>
-      </nav>
+      </AppHeader>
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
