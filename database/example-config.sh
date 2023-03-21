b887a7c891afb6a6a281b1978cdf3619133a9b1f
Mike Dougherty <mikedough213@gmail.com>
2023-03-21 12:02:05 -0500

    Fix example-config.sh

--- database/example-config.sh
+++ database/example-config.sh
@@ -1,7 +1,7 @@
-# change the name of this file to config.sh
-#HOST=cis260db.cwv0o9vfnwsc.us-east-1.rds.amazonaws.com
-HOST=localhost
+# Please rename this file to config.sh
+HOST=cis260db.cwv0o9vfnwsc.us-east-1.rds.amazonaws.com
 PORT=3306
-DATABASE=mdougherty
-USER=root
-PASSWORD=
+DATABASE_NAME=CIS260db
+
+USER=username
+PASSWORD=password
