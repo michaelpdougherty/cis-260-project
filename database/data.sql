0c00cc393168b63202548ce0b7c03947bdab47a8
Mike Dougherty <mikedough213@gmail.com>
2023-03-23 01:03:43 -0500

    Update patient information

--- database/data.sql
+++ database/data.sql
@@ -7,7 +7,7 @@ set @mrn = 1;
 set @st_thomas_aquinas = 'https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRy0Ttfy1cl94ppINrqqfYKIJ__91hbf8VES3WPuxXSJynpTMJ0zXC7HPAo4vNHe-jA';
 set @wilbur_wright = 'https://www.invent.org/sites/default/files/styles/inductee_detail_media/public/inductees/2022-06/Wright%2CW_portrait_edited%20for%20web.jpg?h=fa49f84b&itok=29Jw7s07';
 set @annoying_orange = 'https://i.ytimg.com/vi/DD5UKQggXTc/hqdefault.jpg';
-insert into patients values (@mrn, 'Annoying', 'Orange', 'Sick', 'M', '2002-11-06', 'English', 'Caucasian', 'Mother', '2023-02-06', @provider, @hospital, 'Needs full assistance', @annoying_orange);
+insert into patients values (@mrn, 'Annoying', 'Orange', 'Nostalgic', 'M', '2009-10-09', 'English', 'Caucasian', 'Mother', '2023-02-06', @provider, @hospital, 'Needs full assistance', @annoying_orange);
 insert into alerts (`mrn`, `date_and_time`, `subject`, `status`, `alert_type`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', 'whatever', 'whatever');
 insert into encounters (`mrn`, `date`, `location`, `provider`, `status`, `description`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', @provider, 'whatever', 'whatever');
 insert into labs (`mrn`, `date`, `lab_test`, `value`, `unit`, `abnormal_flag`, `reference_range`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', 'whatever', 'whatever', 'whatever', 'whatever');
@@ -45,7 +45,7 @@ insert into vitals (`mrn`, `date`, `pulse`, `blood_pressure`, `pulse_ox`, `tempe
 
 /* Patient 2 */
 set @mrn = 2;
-insert into patients values (@mrn, 'Wilbur', 'Wright', 'Ill', 'M', '2002-11-06', 'English', 'Caucasian', 'Mother', '2023-02-06', @provider, @hospital, 'Needs full assistance', @wilbur_wright);
+insert into patients values (@mrn, 'Wilbur', 'Wright', 'Ingenious', 'M', '1867-4-16', 'English', 'Caucasian', 'Mother', '2023-02-06', @provider, @hospital, 'Needs full assistance', @wilbur_wright);
 insert into alerts (`mrn`, `date_and_time`, `subject`, `status`, `alert_type`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', 'whatever', 'whatever');
 insert into encounters (`mrn`, `date`, `location`, `provider`, `status`, `description`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', @provider, 'whatever', 'whatever');
 insert into labs (`mrn`, `date`, `lab_test`, `value`, `unit`, `abnormal_flag`, `reference_range`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', 'whatever', 'whatever', 'whatever', 'whatever');
@@ -84,7 +84,7 @@ insert into vitals (`mrn`, `date`, `pulse`, `blood_pressure`, `pulse_ox`, `tempe
 
 /* Patient 3 */
 set @mrn = 3;
-insert into patients values (@mrn, 'St. Thomas', 'Aquinas', 'Ill', 'M', '2002-11-06', 'English', 'Caucasian', 'Mother', '2023-02-06', @provider, @hospital, 'Needs full assistance', @st_thomas_aquinas);
+insert into patients values (@mrn, 'St. Thomas', 'Aquinas', 'Holy', 'M', '1225-1-28', 'English', 'Caucasian', 'Mother', '2023-02-06', @provider, @hospital, 'Needs full assistance', @st_thomas_aquinas);
 insert into alerts (`mrn`, `date_and_time`, `subject`, `status`, `alert_type`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', 'whatever', 'whatever');
 insert into encounters (`mrn`, `date`, `location`, `provider`, `status`, `description`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', @provider, 'whatever', 'whatever');
 insert into labs (`mrn`, `date`, `lab_test`, `value`, `unit`, `abnormal_flag`, `reference_range`) VALUES (@mrn, CURRENT_TIMESTAMP(), 'whatever', 'whatever', 'whatever', 'whatever', 'whatever');
