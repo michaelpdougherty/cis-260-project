9fab4ba77c5fdae89e2a134156ad557ad8ed30b8
Mike Dougherty <mikedough213@gmail.com>
2023-03-27 19:19:25 -0500

    Add basic table

--- frontend/src/routes/tabViews/VitalsTabView.jsx
+++ frontend/src/routes/tabViews/VitalsTabView.jsx
@@ -1,8 +1,36 @@
-import JsonTable from '../../JsonTable';
+// import JsonTable from '../../JsonTable';
 
 const VitalsTabView = ({ vitals }) => {
+  // <JsonTable jsonData={[vitals]} />
+  const vitalEntries = [
+    {
+      time: '10:23',
+      date: '12/22/22',
+    }
+  ];
   return (
-    <JsonTable jsonData={[vitals]} />
+    <table>
+      <thead>
+        <th>
+          Date
+        </th>
+        <th>
+          Time
+        </th>
+      </thead>
+      <tbody>
+      {vitalEntries.map(entry => (
+        <tr>
+          <td>
+            {entry.date}
+          </td>
+          <td>
+            {entry.time}
+          </td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
   );
 };
 
