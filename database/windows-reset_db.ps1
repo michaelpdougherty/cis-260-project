ef622c0d2cf9664c1b5e7e9a6ff2a9e3be633e0a
Ina Buenaventura <ibuenaventura@student.ccc.edu>
2023-03-29 18:24:14 -0500

    Create config file

--- database/windows-reset_db.ps1
+++
@@ -1,2 +0,0 @@
-mysqlsh --user="mdougherty" -p"ookay123" -h"cis260db.cwv0o9vfnwsc.us-east-1.rds.amazonaws.com" -D ibuenaventura --file .\schema.sql
-mysqlsh --user="mdougherty" -p"ookay123" -h"cis260db.cwv0o9vfnwsc.us-east-1.rds.amazonaws.com" -D ibuenaventura --file .\data.sql
