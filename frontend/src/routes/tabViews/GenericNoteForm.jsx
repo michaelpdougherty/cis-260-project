23cb8d4d90eb78c720fe01d59bb108e591cf2d1f
Mike Dougherty <mikedough213@gmail.com>
2023-04-18 19:40:21 -0500

    Nothing

--- frontend/src/routes/tabViews/GenericNoteForm.jsx
+++ frontend/src/routes/tabViews/GenericNoteForm.jsx
@@ -1,15 +1,17 @@
 import { Formik, Form, Field } from 'formik';
 import { NoteFormLabel as Label, SelectField, ButtonBlue, NoteHeader, NoteFieldDiv } from '../../styles';
 
-const GenericNoteForm = ({ handleSubmit, initialValues, noteSchema, isSubmitting }) => {
+const GenericNoteForm = ({ handleSubmit, initialValues, noteSchema }) => {
   const getCurrentField = ({ type, name, options, ...otherProps }) => {
     switch (type) {
       case 'textarea':
         return <Field as='textarea' id={name} name={name} {...otherProps} rows={3} style={{ width: '100%', marginTop: 5 }} />;
       case 'select':
-        return <SelectField as='select' id={name} name={name} {...otherProps}>
-          {Object.entries(options).map(([optionKey, optionValue]) => <option key={`${name}.${optionKey}.option`} value={optionValue}>{optionValue}</option>)}
-        </SelectField>;
+        return (
+          <SelectField type='select' as='select' id={name} name={name} {...otherProps}>
+            {Object.entries(options).map(([optionKey, optionValue]) => <option key={optionKey} value={optionValue}>{optionValue}</option>)}
+          </SelectField>
+        );
       default:
         return <Field type={type ?? 'text'} id={name} name={name} {...otherProps} />;
     }
@@ -21,17 +23,17 @@ const GenericNoteForm = ({ handleSubmit, initialValues, noteSchema, isSubmitting
         <Form>
           {noteSchema.header && <NoteHeader>{noteSchema.header}</NoteHeader>}
           {noteSchema.fieldSets ? noteSchema.fieldSets.map(fieldSet => (
-            <fieldset style={{ padding: 2 }} key={`${fieldSet.legend}.fieldset`}>
+            <fieldset style={{ padding: 2 }} key={fieldSet.legend}>
               <legend>{fieldSet.legend}</legend>
               {fieldSet.fields.map(field => (
-                <NoteFieldDiv key={`${fieldSet.legend}.${field.name}.NoteFieldDiv`} direction={field.type === 'textarea' ? 'column' : 'row'}>
+                <NoteFieldDiv key={field.name} direction={field.type === 'textarea' ? 'column' : 'row'}>
                   { field.header && <Label htmlFor={field.name}>{field.header}:</Label> }
                   {getCurrentField(field)}
                 </NoteFieldDiv>
               ))}
             </fieldset>
           )) : noteSchema.fields.map(field => (
-              <NoteFieldDiv key={`${field.name}.NoteFieldDiv`} direction={field.type === 'textarea' ? 'column' : 'row'}>
+              <NoteFieldDiv key={field.name} direction={field.type === 'textarea' ? 'column' : 'row'}>
                 { field.header && <Label htmlFor={field.name}>{field.header}:</Label> }
                 {getCurrentField(field)}
               </NoteFieldDiv>
