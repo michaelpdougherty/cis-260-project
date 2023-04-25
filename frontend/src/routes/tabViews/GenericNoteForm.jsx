bdf55674c47185edf2a428f877be6d996d7733aa
Mike Dougherty <mikedough213@gmail.com>
2023-04-25 13:06:06 -0500

    Fix console errors

--- frontend/src/routes/tabViews/GenericNoteForm.jsx
+++ frontend/src/routes/tabViews/GenericNoteForm.jsx
@@ -5,7 +5,7 @@ const GenericNoteForm = ({ handleSubmit, initialValues, noteSchema }) => {
   const getCurrentField = ({ type, name, options, ...otherProps }) => {
     switch (type) {
       case 'textarea':
-        return <Field as='textarea' id={name} name={name} {...otherProps} rows={3} style={{ width: '100%', marginTop: 5 }} />;
+        return <Field as='textarea' id={name} name={name} rows={3} style={{ width: '100%', marginTop: 5 }} {...otherProps} />;
       case 'select':
         return (
           <SelectFieldDiv>
@@ -15,23 +15,19 @@ const GenericNoteForm = ({ handleSubmit, initialValues, noteSchema }) => {
           </SelectFieldDiv>
         );
       default:
-        return <Field type='text' id={name} name={name} />;
-        /*
         return <Field style={{
           width: '50%',
         }} type={type ?? 'text'} id={name} name={name} {...otherProps} />;
-        */
     }
   };
 
-  let i = 0;
   return (
     <Formik initialValues={initialValues} onSubmit={handleSubmit} enableReinitialize>
       {({ isSubmitting }) => (
         <Form>
           {noteSchema.header && <NoteHeader>{noteSchema.header}</NoteHeader>}
           {noteSchema.fieldSets ? noteSchema.fieldSets.map(fieldSet => (
-            <fieldset style={{ padding: 2 }} key={fieldSet.legend ?? 'fieldset-' + i++}>
+            <fieldset style={{ padding: 2 }} key={fieldSet.legend}>
               <legend>{fieldSet.legend}</legend>
               {fieldSet.fields.map(field => (
                 <NoteFieldDiv key={field.name} direction={field.type === 'textarea' ? 'column' : 'row'}>
